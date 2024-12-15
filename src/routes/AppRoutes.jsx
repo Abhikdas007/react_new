d3321e9b946065a9b4ca9b54af9e8350e3236a1c
Abhik <magnusabhik789@gmail.com>
2024-12-15 23:36:31 +0530

    errro solved

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {Routes, Route} from 'react-router';
 import Home from "../pages/Home";
 import About from "../pages/About";
-import Contact from "../pages/contact";
+import Contact from "../pages/Contact";
 
 const AppRoutes =()=>{
     return(
